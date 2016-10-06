c1114bf4692bae3599ae0984d121a04df2821f5e
Sebastian_Quast <Strike.in@googlemail.com>
2016-10-06 18:14:59 +0200

    Das ist ein Test

--- app/src/main/java/omgproduction/com/dsport_application/MainActivity.java
+++ app/src/main/java/omgproduction/com/dsport_application/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TEST
+        // Das ist von Sebastian !
     }
 }
